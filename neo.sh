9680370f1e595c8f8c45e61eec50e9d77353287a
Moritz Schepp <moritz.schepp@gmail.com>
2020-04-28 12:08:05 -0700

    attempt to fix for paths with spaces

--- neo.sh
+++ neo.sh
@@ -4,5 +4,5 @@ docker run --rm -ti \
   --publish=7474:7474 \
   --publish=7687:7687 \
   --env=NEO4J_AUTH=none \
-  --volume=$(pwd)/../cache/neo_data:/data \
+  --volume="$(pwd)/../cache/neo_data:/data" \
   neo4j:3.5.15
